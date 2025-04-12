5def67bae3c7f6d28f6b480b35b62d0492013896
shubhamraj-16 <147704855+shubhamraj-16@users.noreply.github.com>
2025-04-12 17:58:15 +0530

    refactor(emailService): update transporter configuration for better security
    The transporter configuration in emailService.js was updated to include explicit host, port, and secure settings for enhanced security. Additionally, dotenv was added to manage environment variables more effectively.

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,8 +1,13 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 
 // Create a transporter
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
@@ -78,4 +83,4 @@ const sendWelcomeEmail = async (email, username) => {
 module.exports = {
   sendPasswordRecoveryEmail,
   sendWelcomeEmail
-}; 
+};
